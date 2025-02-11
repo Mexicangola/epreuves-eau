dae6638470200a20ee15f13d6f50867018b2ccfc
Ana <gutierrez.moyano.ana@gmail.com>
2025-02-11 11:28:48 +0100

    mise a jour eau02

--- eau02.java
+++ eau02.java
@@ -1,22 +1,25 @@
-import java.util.Arrays;
+// Créez un programme qui affiche ses arguments reçus à l’envers.
 
 public class eau02
 { 
     public static void main(String[] args) 
     {
-        System.out.println(args.length);
-
-        for(int i = (args.length-1) ; i >= 0 ; i--){
-            System.out.println(args[i] + " ");
+        for(int i = args.length-1 ; i >= 0 ; i--){
+            System.out.println(args[i]);
         }
     }
 }
 
-/*
-        String[] sArgs = String.join(" ", args);
-        String[] tArgs = sArgs.split(" ");
+/*ROUND ONE
+        String[] arguments = new String [args.length];
+        int index = 0;
 
-        for(int i = (tArgs.length - 1) ; i >= 0 ; i--){
-            System.out.println(tArgs[i] + " ");
+        for(int i = (args.length-1) ; i >= 0 ; i--){
+            arguments[index] = args[i];
+            index++;
         }
-*/
+
+        for(String mot : arguments){
+            System.out.println(mot);
+        }
+ */
