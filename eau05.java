fe4f18b936772706c7b830123b26b5ea47773814
Ana <gutierrez.moyano.ana@gmail.com>
2025-02-14 12:52:47 +0100

    mise a jour eau05, avec méthodes : charAt(), .equals(), .substring(debut, fin) et String.format

--- eau05.java
+++ eau05.java
@@ -1,9 +1,39 @@
-import java.util.ArrayList;
+// Créez un programme qui détermine si une chaîne de caractère se trouve dans une autre.
 
 public class eau05
 { 
     public static void main(String[] args)
-    {
+    {        
+        if(args.length != 2){
+            System.out.println("donne moi deux arguments separés par des guillemets. Exemple : 'Hello' 'world!'");
+            return;
+        }
+
+        if(args[0].length() < args[1].length()){
+            subString(args[0], args[1]);
+        } else{
+            subString(args[1], args[0]);
+        }
+    }
+
+    public static void subString(String petitTexte, String grandTexte){
+        for(int i = 0 ; i < (grandTexte.length() - petitTexte.length()) ; i++){
+            if(petitTexte.charAt(0) == grandTexte.charAt(i)){
+                String substringGrandTexte = grandTexte.substring(i, (i + petitTexte.length()));
+                if(substringGrandTexte.equals(petitTexte)){
+                    String message2 = String.format("on trouve dans l'index %d le substring : '%s' du texte '%s'", i , substringGrandTexte, grandTexte);
+                    System.out.println(message2);
+                    return;
+                }
+            }
+        }
+
+        String message3 = String.format("on ne trouve pas le substring '%s' dans le texte '%s'", petitTexte, grandTexte);
+        System.out.println(message3);
+    }
+}
+
+/* ROUND ONE
         String[] texteSplit1 = (args[0]).split("");
         String[] texteSplit2 = (args[1]).split("");
         int taille = (args[1]).length();
@@ -25,5 +55,4 @@ public class eau05
                 }
             }
         }
-    }
-}
+*/
